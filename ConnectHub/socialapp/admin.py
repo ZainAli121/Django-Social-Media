4a5185228e174d68fc3839101558624815f6916b
Zain <learnerstudent001@gmail.com>
2023-11-13 12:38:36 +0500

    feat: models done

--- ConnectHub/socialapp/admin.py
+++ ConnectHub/socialapp/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Tag)
--- ConnectHub/socialapp/models.py
+++ ConnectHub/socialapp/models.py
@@ -6,9 +6,48 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     username = models.CharField(max_length=200, unique=True, null=True)
     email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(null=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_pic = models.ImageField(null=True, default="avatar.svg") 
 
     def __str__(self):
         return self.username
 
 
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    desc = models.TextField(null=True, blank=True)
+    tag = models.OneToOneField('Tag', on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True,blank=True,default="default.jpg")
+
+    def __str__(self):
+        return f"Post by {self.owner.username}"
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by {self.owner.username} on {self.post}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
