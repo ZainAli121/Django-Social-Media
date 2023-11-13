22d4a8318cfd6ec48fe567f66a5939b91a328ceb
Zain <learnerstudent001@gmail.com>
2023-11-13 10:34:05 +0500

    custom user model

--- ConnectHub/socialapp/models.py
+++ ConnectHub/socialapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    username = models.CharField(max_length=200, unique=True, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    def __str__(self):
+        return self.username
+
+
