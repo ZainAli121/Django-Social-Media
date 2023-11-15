724e08b81c1c830337b89c30be27be3b7786f01c
Zain <learnerstudent001@gmail.com>
2023-11-15 15:44:48 +0500

    Authentication Done

--- ConnectHub/socialapp/urls.py
+++ ConnectHub/socialapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.loginUser, name='login'),
+    path('register/', views.signupUser, name='signup'),
+    path('logout/', views.logoutUser, name='logout'),
+    
 ]
--- ConnectHub/socialapp/views.py
+++ ConnectHub/socialapp/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import *
-# Create your views here.
+from django.contrib import messages
+from .forms import *
+
 def home(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'socialapp/home.html', context)
+
+def loginUser(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        user = User.objects.get(username = username)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    context = {'page': page}
+    return render(request, 'socialapp/register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
+def signupUser(request):
+    form = MyUserForm()
+
+    if request.method == 'POST':
+        form = MyUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        
+    context = {'form': form}
+    return render(request, 'socialapp/register.html', context)
+
