0e4a38f60a48284279502875d0499e0effe6d4ae
Zain <learnerstudent001@gmail.com>
2023-11-28 20:24:54 +0500

    Restful API for post CRUD

--- ConnectHub/socialapp/api/views.py
+++ ConnectHub/socialapp/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from socialapp.models import *
 from .serializers import *
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET', 'POST']) # This decorator allows us to use function based views instead of class based views
 def getRoutes(request):
@@ -17,17 +19,47 @@ def getRoutes(request):
 
     return Response(routes)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def getPosts(request):
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many=True) # many = True means we are serializing multiple objects
-    return Response(serializer.data)
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True) # many = True means we are serializing multiple objects
+        return Response(serializer.data)
 
-@api_view(['GET'])
+    elif request.method == 'POST':
+        data = request.data.copy()
+        data['owner'] = request.user.id
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"error": "Invalid data", "details": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def getPost(request, pk):
-    post = Post.objects.get(id=pk)
-    serializer = PostSerializer(post, many=False)
-    return Response(serializer.data)
+    try:
+        post = Post.objects.get(id=pk)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = PostSerializer(post, many=False)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = request.data.copy()
+        data['owner'] = request.user.id
+        serializer = PostSerializer(instance=post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response({"error": "Invalid data", "details": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response({"success": "Post deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+    
 
 @api_view(['GET'])
 def getProfile(request, pk):
